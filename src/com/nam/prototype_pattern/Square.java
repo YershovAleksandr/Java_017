63f4fa754f318113bd199c5417077773b88e1e2e
hp <namstudionsk@gmail.com>
2018-09-16 12:33:31 +0700

    Add Square Class

---
+++ src/com/nam/prototype_pattern/Square.java
@@ -0,0 +1,13 @@
+package com.nam.prototype_pattern;
+
+public class Square extends Shape {
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw(){
+        System.out.println("Inside Square::draw method.");
+    }
+}
