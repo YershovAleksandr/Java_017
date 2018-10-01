69eec6d167439eaa65e9a027d0d827941de1aa25
hp <namstudionsk@gmail.com>
2018-10-01 18:22:40 +0700

    Add Observer Pattern

---
+++ src/com/nam/observer_pattern/HexaObserver.java
@@ -0,0 +1,13 @@
+package com.nam.observer_pattern;
+
+public class HexaObserver extends Observer {
+    public HexaObserver(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update(){
+        System.out.println("Hex String : " + Integer.toHexString(subject.getState()).toUpperCase());
+    }
+}
---
+++ src/com/nam/observer_pattern/Observer.java
@@ -0,0 +1,6 @@
+package com.nam.observer_pattern;
+
+public abstract class Observer {
+    protected Subject subject;
+    public abstract void update();
+}
