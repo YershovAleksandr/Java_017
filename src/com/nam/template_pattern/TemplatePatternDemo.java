d94c3059771baa90f883e4bf04300aa6ff8a9661
hp <namstudionsk@gmail.com>
2018-10-06 19:30:14 +0700

    Add Template Pattern

---
+++ src/com/nam/template_pattern/TemplatePatternDemo.java
@@ -0,0 +1,11 @@
+package com.nam.template_pattern;
+
+public class TemplatePatternDemo {
+    public static void main(String[] args){
+        Game game = new Cricket();
+        game.play();
+        System.out.println();
+        game = new Football();
+        game.play();
+    }
+}
