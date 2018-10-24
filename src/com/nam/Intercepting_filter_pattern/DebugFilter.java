8c09ba2b005b863efdf1808a3b1f07090d802b76
hp <namstudionsk@gmail.com>
2018-10-24 23:47:25 +0700

    Add Data Access Object Pattern

---
+++ src/com/nam/Intercepting_filter_pattern/DebugFilter.java
@@ -0,0 +1,8 @@
+package com.nam.Intercepting_filter_pattern;
+
+public class DebugFilter implements Filter {
+    @Override
+    public void execute(String request){
+        System.out.println("request log : " + request);
+    }
+}
